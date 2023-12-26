7e91e0439d506f7ebec737668dc9a0e9a2e0fc66
William f Jing <william.f.jing@cn.pwc.com>
2023-12-26 15:37:20 +0800

    update

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,6 +1,7 @@
-export { default as Avatar } from './avatar'
-export { default as Button } from './button'
-export { default as Divider } from './divider'
-export { default as Empty } from './empty'
-export { default as Input } from './input'
-export { default as Spin } from './spin'
+export { default as Avatar } from "./avatar";
+export { default as Button } from "./button";
+export { default as Divider } from "./divider";
+export { default as Empty } from "./empty";
+export { default as Input } from "./input";
+export { default as Spin } from "./spin";
+export { default as Dropdown } from "./menu/Dropdown";
